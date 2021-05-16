17789ff091807765c7d3f62c1ece624182a62dbc
Eric Sandine <ericsandine@gmail.com>
2021-05-16 10:52:01 -0700

    init

---
+++ flaskr/__init__.py
@@ -0,0 +1,38 @@
+import os
+
+from flask import Flask
+from flask_mongoengine import MongoEngine
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+    app.config['MONGODB_SETTINGS'] = {
+        'db': 'hikerblog',
+        'host': 'localhost',
+        'port': 27017
+    }
+    db = MongoEngine()
+    db.init_app(app)
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app
